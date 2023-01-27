8c09f23d2376d07ffbc714f6803008d649980277
Pedro Furlan <pedrofurlan6@gmail.com>
2023-01-27 09:40:24 -0300

    Update index.ts

--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -6,7 +6,7 @@ import {
   fontWeights,
   lineHeights,
   space,
-} from '@ignite-ui/tokens'
+} from '@pedrofurlan-ignite-ui/tokens'
 import { createStitches, defaultThemeMap } from '@stitches/react'
 
 export const {
